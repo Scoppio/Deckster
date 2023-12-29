12a5a67ab9652365adb1e337d2cdf7575bf67b1a
FredOliveiraSantos <oliveira.fredsantos@gmail.com>
2023-12-29 11:10:40 -0300

    feat: fixing code to create executable via electron:build command

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // import { useState, useMemo } from 'react'
 
-import { GameArena } from 'renderer/GameArena'
-import { amalia_walker_text, uw_karn_control_text, convertTextToDeck } from 'commons/DeckLoader'
-import { GameStateController } from 'controllers/GameStateController'
-import { Player } from 'commons/Player'
+import { GameArena } from './renderer/GameArena'
+import { amalia_walker_text, uw_karn_control_text, convertTextToDeck } from './commons/DeckLoader'
+import { GameStateController } from './controllers/GameStateController'
+import { Player } from './commons/Player'
 import '@atlaskit/css-reset'
 import 'mana-font/css/mana.css'
 
@@ -55,4 +55,4 @@ function App() {
 
 }
 
-export default App
+export default App;
