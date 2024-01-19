9731f9133eb60e32ef27d7e9b4a7e5becf110cb9
Lucas Coppio <lucascoppio@gmail.com>
2024-01-18 23:58:24 -0300

    feat: update sderver

--- nodeserver/server.js
+++ nodeserver/server.js
@@ -2,6 +2,10 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 8000 });
 
+const game = {
+  players: {}
+}
+
 wss.on('connection', ws => {
   console.log('WebSocket connection established');
 
@@ -11,16 +15,29 @@ wss.on('connection', ws => {
 
     // Respond to the "draw_card" event
     if (event.type === 'draw_card') {
-      const response = {
+      const player_id = event.sender['id']
+      const player = game.players[player_id]
+      const num_cards = event.payload['number_of_cards']
+      const zone = event.payload['zone']
+      let cards_drawn = 0
+      for (let i = 0; i < num_cards; i++) {
+        if (player[zone].length <= 0) {
+          break
+        }
+        player['hand'].push(player[zone].pop())
+        cards_drawn += 1
+      }
+
+      const responseLog = {
         type: '_logEvent',
-        payload: "draw_card",
+        payload: "Player " + player['name'] + " drew " + cards_drawn + " cards from " + zone,
         sender: "server"
       };
 
-      ws.send(JSON.stringify(response));
+      ws.send(JSON.stringify(responseLog));
+      ws.send(JSON.stringify({type: '_drawCard', payload: player, sender: "server"}));
       
-      console.log('sending: %s', response);
-      ws.send(JSON.stringify({...response, type: '_drawCard'}));
+      console.log('sending: %s', responseLog);
     }
 
     // Respond to the "untap_all" event
@@ -36,9 +53,10 @@ wss.on('connection', ws => {
 
     // Respond to the "login_player" event
     if (event.type === 'login_player') {
+      game.players[event.payload['id']] = event.payload
       const response = {
         type: '_logEvent',
-        payload: "login_player",
+        payload: "Player " + event.payload['name'] + " logged in",
         sender: "server"
       };
 
