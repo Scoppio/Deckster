7731bc1a5658ff2f95e606c437403a59661d3f52
Coppio <Lucas.Coppio@syngenta.com>
2024-03-14 22:30:55 -0300

    feat: added card detail

--- src/renderer/GameStateBoard.js
+++ src/renderer/GameStateBoard.js
@@ -59,3 +59,47 @@ GameStateBoard.propTypes = {
   gameState: PropTypes.object.isRequired,
   playerRef: PropTypes.object.isRequired,
 };
+
+const CardDetail = ({ gameState }) => {
+  return (
+    <div>
+      {gameState.focus_card ? (
+        <>
+          <Row>
+            <h2
+              style={{
+                fontSize: "1.5vw",
+                whiteSpace: "nowrap",
+                overflow: "visible",
+                textOverflow: "ellipsis",
+              }}
+            >
+              {gameState.focus_card.card_name}{" "}
+              {gameState.focus_card.card_mana_cost}
+            </h2>
+          </Row>
+          <Row>
+            <img
+              src={gameState.focus_card.card_image_uris.normal}
+              alt={gameState.focus_card.card_name}
+              style={{ width: "100%" }}
+            />
+          </Row>
+          <Row>
+            <p>{gameState.focus_card.card_type_line}</p>
+          </Row>
+          <Row>
+            <p>{gameState.focus_card.card_read_oracle_text}</p>
+          </Row>
+          <Row>
+            <p>{gameState.focus_card.power_toughness}</p>
+          </Row>
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+CardDetail.propTypes = {
+  gameState: PropTypes.object.isRequired,
+};
